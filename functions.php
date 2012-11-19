dd1bbd28da88f22d3bad443f4282b9f6da403608
SPolischook <spolischook@gmail.com>
2012-11-19 16:05:48 +0200

    Replace manu and make it more usability

---
+++ functions.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by Sergey Polischook.
+ */
+function aries_setup() {
+    add_theme_support( 'post-formats', array( 'gallery' ) );
+}
+add_action( 'after_setup_theme', 'aries_setup' );
+
+// свой класс построения меню:
+class magomra_walker_nav_menu extends Walker_Nav_Menu {
+    function end_el( &$output, $item, $depth = 0, $args = array() ) {
+        $output .= "<ul class='sub-nav-menu'>\n";
+          query_posts( 'cat='.$item->object_id );
+             // The Loop
+            while ( have_posts() ) : the_post();
+                $output.= '<li>';
+                $output.= the_title('','',false);
+                $output.= '</li>';
+            endwhile;
+
+            // Reset Query
+            wp_reset_query();
+        $output .= "</ul>\n";
+
+        $output .= "</li>\n";
+//        print_r($item); exit;
+    }
+}
