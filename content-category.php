48cccf968c5bd52daca26565c06c6452e326d459
SPolischook <spolischook@gmail.com>
2012-12-12 03:34:25 +0200

    add customizible category files

---
+++ content-category.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying posts in the Image post format
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <h2><?php the_title(); ?></h2>
+		<div class="entry-content another">
+			<?php
+            global $more;
+            $more = false;
+            var_dump(get_the_content( __( '', 'twentytwelve' ), false ));
+            $more = true;
+            ?>
+		</div><!-- .entry-content -->
+
+		<footer class="entry-meta">
+			<?php if ( comments_open() ) : ?>
+			<div class="comments-link">
+				<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentytwelve' ) . '</span>', __( '1 Reply', 'twentytwelve' ), __( '% Replies', 'twentytwelve' ) ); ?>
+			</div><!-- .comments-link -->
+			<?php endif; // comments_open() ?>
+			<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
+		</footer><!-- .entry-meta -->
+	</article><!-- #post -->
