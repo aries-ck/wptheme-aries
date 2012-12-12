35693c05a1ef8bab34490c1ba47e29120c0cc9ac
SPolischook <spolischook@gmail.com>
2012-12-12 03:38:19 +0200

    made view sidebar after desc of post

--- sidebar.php
+++ sidebar.php
@@ -12,7 +12,7 @@
 
 	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 		<div id="secondary" class="widget-area" role="complementary">
-			<?php dynamic_sidebar( 'sidebar-1' ); ?>
             <?php the_content(null, true); ?>
+            <?php dynamic_sidebar( 'sidebar-1' ); ?>
 		</div><!-- #secondary -->
 	<?php endif; ?>
