9fba5467e6ecbb29200396ea082d1c134a269fb6
SPolischook <spolischook@gmail.com>
2012-12-16 00:06:02 +0200

    moveon category description

--- sidebar.php
+++ sidebar.php
@@ -12,7 +12,11 @@
 
 	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 		<div id="secondary" class="widget-area" role="complementary">
+        <?php if ( category_description() ) { // Show an optional category description ?>
+            <div class="archive-meta"><?php echo category_description(); ?></div>
+            <?php } else{ ?>
             <?php the_content(null, true); ?>
             <?php dynamic_sidebar( 'sidebar-1' ); ?>
+        <?php } ?>
 		</div><!-- #secondary -->
 	<?php endif; ?>
