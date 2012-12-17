596bb037538657f4412e92a46878cb7283a9ea92
SPolischook <spolischook@gmail.com>
2012-12-17 19:12:13 +0200

    Add bread crumbs for a single post

--- single.php
+++ single.php
@@ -11,7 +11,8 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-
+            <?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs();
+            ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'content', get_post_format() ); ?>
