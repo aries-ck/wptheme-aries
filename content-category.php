6d1f8ceadf14505db72896273c1b7ea49228f998
SPolischook <spolischook@gmail.com>
2012-12-17 19:11:19 +0200

    add if statment for a sale content with custom template for content

--- content-category.php
+++ content-category.php
@@ -8,23 +8,27 @@
  */
 ?>
 
-			<?php
-            $content = get_the_content( __( '', 'twentytwelve' ), false );
-            //Explode gallery
-            $aContent = explode('[gallery ids="', $content);
-            //True if gallery isset in the post
-            if (count($aContent) > 1) {
-                //Find first id at the gallery
-                $aContent = explode(',', $aContent[1]);
-                $aImg = wp_get_attachment_image_src($aContent[0]);
-                //Echo first image
-                echo "
-                    <div style='float: left; padding-right: 10px;'>
-                        <a href='".get_permalink()."'>
-                            <img src='".$aImg[0]."' style=''>
-                            <h3 align='center'>".the_title('','', false)."</h3>
-                        </a>
-                    </div>
-                ";
-            }
-            ?>
+<?php
+if (in_category('sale')) {
+    get_template_part( 'content');
+}
+else {
+    $content = get_the_content( __( '', 'twentytwelve' ), false );
+    //Explode gallery
+    $aContent = explode('[gallery ids="', $content);
+    //True if gallery isset in the post
+    if (count($aContent) > 1) {
+        //Find first id at the gallery
+        $aContent = explode(',', $aContent[1]);
+        $aImg = wp_get_attachment_image_src($aContent[0]);
+        //Echo first image
+        echo "
+            <div style='float: left; padding-right: 10px;'>
+                <a href='".get_permalink()."'>
+                    <img src='".$aImg[0]."' style=''>
+                    <h3 align='center'>".the_title('','', false)."</h3>
+                </a>
+            </div>
+        ";
+    }
+}?>
